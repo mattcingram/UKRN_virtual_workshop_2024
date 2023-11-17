a88e7c1c5be01be5029bc45fbeb38c3b62468565
Matt Ingram <40709229+mattcingram@users.noreply.github.com>
2023-11-17 10:18:51 -0500

    Update install.R

--- install.R
+++ install.R
@@ -1,2 +1,5 @@
-library("tidyverse")
-library("writexl")
+# packages 
+mypackages <- c("tidyverse", "writexl")
+install.packages(mypackages, verbose=TRUE)
+# alternative syntax, or to install additional packages
+install.packages(c("pacman"))
